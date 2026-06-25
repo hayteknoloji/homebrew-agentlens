d68ba62d76aadab9bfbc727bdda7d7462cc69ed2
Arif AKTAŞ <arif.emre.aktas@gmail.com>
2026-06-25 14:40:05 +0300

    agentlens 0.1.0

---
+++ Casks/agentlens.rb
@@ -0,0 +1,15 @@
+cask "agentlens" do
+  version "0.1.0"
+  sha256 "aea32b1210f106c237a1288097786e3fe7a6a29d906f6edfd057b6b785394d00"
+
+  url "https://github.com/hayteknoloji/homebrew-agentlens/releases/download/v#{version}/AgentLens-#{version}.zip"
+  name "AgentLens"
+  desc "Claude Code & OpenAI Codex yerel kullanım metrikleri (menü çubuğu uygulaması)"
+  homepage "https://github.com/hayteknoloji/homebrew-agentlens"
+
+  depends_on macos: :sonoma
+
+  app "AgentLens.app"
+
+  zap trash: "~/Library/Application Support/AgentLens"
+end
