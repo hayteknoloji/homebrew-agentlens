0bdfc9f9d76277671ec19e01787902eb8b30e22e
Arif AKTAŞ <arif.emre.aktas@gmail.com>
2026-06-30 16:07:34 +0300

    agentlens 0.1.19

--- Casks/agentlens.rb
+++ Casks/agentlens.rb
@@ -1,6 +1,6 @@
 cask "agentlens" do
-  version "0.1.18"
-  sha256 "b4c92404fca51e92eb8a18db6f332244041e6a49fb492cc15584e9a30c589b78"
+  version "0.1.19"
+  sha256 "897a733e22357d5806f4f2fe6204ccc559bc76423ba04ea123adde548563c352"
 
   url "https://github.com/hayteknoloji/homebrew-agentlens/releases/download/v#{version}/AgentLens-#{version}.zip"
   name "AgentLens"
